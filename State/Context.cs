b67447d5286c360f2d673d305458bea0e30c894f
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-04-09 19:58:14 -0300

    Add project files.

---
+++ State/Context.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class Context
+    {
+        private State _state;
+
+        public Context(State state)
+        {
+            State = state;
+        }
+
+        public State State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine($"estado: {_state.GetType().Name}");
+            }
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+    }
+}
